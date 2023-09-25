70658a8ba0bc1c5707d95b0c74991dda4d14f8aa
“hinata0106” <karahina2002@gmail.com>
2023-09-25 16:01:16 +0900

    タグ機能を消した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_23_081240) do
+ActiveRecord::Schema.define(version: 2023_08_19_142746) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "post_id", null: false
@@ -19,15 +19,6 @@ ActiveRecord::Schema.define(version: 2023_08_23_081240) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_likes_on_post_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
-  end
-
-  create_table "post_tag_relations", force: :cascade do |t|
-    t.integer "post_id", null: false
-    t.integer "tag_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id"], name: "index_post_tag_relations_on_post_id"
-    t.index ["tag_id"], name: "index_post_tag_relations_on_tag_id"
   end
 
   create_table "post_tags", force: :cascade do |t|
@@ -50,12 +41,6 @@ ActiveRecord::Schema.define(version: 2023_08_23_081240) do
     t.string "category"
   end
 
-  create_table "tags", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -72,8 +57,6 @@ ActiveRecord::Schema.define(version: 2023_08_23_081240) do
 
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
-  add_foreign_key "post_tag_relations", "posts"
-  add_foreign_key "post_tag_relations", "tags"
   add_foreign_key "post_tags", "registers"
   add_foreign_key "post_tags", "tags"
 end
